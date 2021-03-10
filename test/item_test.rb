85ad0c52a3bac15a7a20f14535c4b500044278f5
r-stemmle <ryan.stemmle@gmail.com>
2021-03-10 12:27:39 -0500

    Add item class and tests

--- test/item_test.rb
+++ test/item_test.rb
@@ -5,4 +5,18 @@ require 'pry'
 
 
 class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item = Item.new({name: "Peach", price: "$0.50"})
+
+    assert_instance_of Item, item
+  end
+
+  def test_it_has_attributes
+    item = Item.new({name: "Peach", price: "$0.50"})
+
+    assert_equal "Peach", item.name
+    assert_equal 0.5, item.price
+  end
+
 end
